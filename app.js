07c1f73e25038c4c57919f5be45ac67087bb259e
Austin Matthews <amatthews@concentric.life>
2023-07-13 08:41:00 -0700

    More styling + javascript gsap animation

---
+++ app.js
@@ -0,0 +1,50 @@
+const navButton = document.querySelector('.nav-button');
+const navOpen = document.querySelector('.nav-open');
+
+const t1 = new TimelineLine({ paused: true, reversed: true });
+
+t1.to(".cover", 1, {
+    width: "60%",
+    ease: Power2.easeOut
+})
+    .to(
+        "nav",
+        1,
+        {
+            height: "100",
+            ease: Power2.easeOut
+        },
+        '-= 0.5'
+    )
+    .fromTo(
+        '.nav-open',
+        0.5,
+        {
+            opacity: 0,
+            x: 50,
+            ease: Power2.easeOut
+        },
+        {
+            opacity: 1,
+            x: 0,
+            onComplete: function() {
+                navOpen.style.pointerEvents = "auto";
+                console.log("Hit it brotha");
+            }
+        }
+    );
+
+navButton.addEventListener("click", () => {
+    if(t1.iasActive()){
+        e.preventDefault();
+        e.stopimmediatePropagation();
+        return false;
+    }
+
+    toggleTween(t1)
+});
+
+
+function toggleTween(tween){
+    tween.reversed() ? tween.play() : tween.reverse();
+}
